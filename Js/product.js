a2f54aa6aba34b3955ae26db9fdd1ad40fa79d00
aliynajjar <ali.y.alnajjar@gmail.com>
2023-03-04 22:31:57 +0200

    Added ES6 functions to counter

--- Js/product.js
+++ Js/product.js
@@ -1,14 +1,14 @@
-function increaseCount(a, b) {
-  var input = b.previousElementSibling;
-  var value = parseInt(input.value, 10);
+const increaseCount = (a,b) =>{
+  let input = b.previousElementSibling;
+  let value = parseInt(input.value, 10);
   value = isNaN(value) ? 0 : value;
   value++;
   input.value = value;
 }
 
-function decreaseCount(a, b) {
-  var input = b.nextElementSibling;
-  var value = parseInt(input.value, 10);
+const decreaseCount = (a,b) => {
+  let input = b.nextElementSibling;
+  let value = parseInt(input.value, 10);
   if (value > 1) {
     value = isNaN(value) ? 0 : value;
     value--;
