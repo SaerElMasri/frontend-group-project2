d2025d20020b405fb37213579c3c92162fa16416
aliynajjar <ali.y.alnajjar@gmail.com>
2023-03-04 17:28:50 +0200

    Added sections to homepage and styled it

--- Js/homepage.js
+++ Js/homepage.js
@@ -17,3 +17,20 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+let countDownDate = new Date("Mar 10, 2023 15:37:25").getTime();
+let x = setInterval(function() {
+  let now = new Date().getTime();
+  let distance = countDownDate - now;
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("hot_deal_countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  document.getElementById("hot_deal_countdown").innerHTML = `${days} Days ${hours} Hours ${minutes} Minutes ${seconds} Seconds`;
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("hot_deal_countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
