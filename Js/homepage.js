0c0f4383e02b679399a5f80417451b95a162e6b5
aliynajjar <ali.y.alnajjar@gmail.com>
2023-03-04 21:47:24 +0200

    Added new styling and slideshows

--- Js/homepage.js
+++ Js/homepage.js
@@ -3,7 +3,7 @@ showSlides();
 
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
+  let slides = document.getElementsByClassName("slides");
   let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
@@ -13,9 +13,9 @@ function showSlides() {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "flex";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 10000); // Change image every 2 seconds
 }
 
 let countDownDate = new Date("Mar 10, 2023 15:37:25").getTime();
