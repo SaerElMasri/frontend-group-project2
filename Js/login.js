5dc6ca5ab4638cdb9560eff561805f3ac304982b
rima <rima.abouchakra.gms@gmail.com>
2023-03-06 01:33:40 +0200

    added register relation to database

--- Js/login.js
+++ Js/login.js
@@ -31,3 +31,64 @@ Signupbtn.addEventListener("click", e => {
      LoginForm.style.display = "none";
 
 });
+
+
+let signup_btn = document.getElementById('btn_signup');
+let signin_btn = document.getElementById('btn_login')
+signup_btn.addEventListener('click', signup);
+signin_btn.addEventListener('click', signin);
+
+function signup() {
+
+    let first_name = document.getElementById('first_signup').value;
+    let last_name = document.getElementById('last_signup').value;
+    let email = document.getElementById('email_signup').value;
+    let password = document.getElementById('password_signup').value;
+    // let confirm_password = document.getElementById('confirmPass_signup').value;
+
+    let data = new FormData();
+    
+    data.append('first_name', first_name);
+    data.append('last_name', last_name);
+    data.append('email', email);
+    data.append('password', password);
+    // axios.post('http://localhost/backend-group-project2/signup.php', data).then(function (res) {
+   
+    //   alert("signed up")
+    //   console.log(res.data)   
+    //   console.log('signed up successfully')
+ 
+    // }).catch((err)=> {
+    //     console.log(err);
+    //      alert("failed")
+    // })
+    axios.post('http://localhost/backend-group-project2/signup.php', data)
+    .then((result) => {
+    console.log(result);
+    if (result.data.status=== "success") {
+      alert("Signed up successfully!");
+      window.location.href = "../index.html"; // redirect to product page
+    } else {
+      console.error("Error");
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+}
+
+function signin() {
+    let email = document.getElementById('email_signin').value;
+    let password = document.getElementById('password_signin').value;
+    let data = new FormData();
+    data.append('email', email);
+    data.append('password', password);
+    axios.post('http://localhost/backend-group-project2/login.php', data).then(function (res) {
+        console.log(res.data)
+
+    }).catch(function (err) {
+        console.log(err);
+    })
+
+}
