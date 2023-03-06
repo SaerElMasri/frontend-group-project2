9835a0c42c4e2f250ae4e449341853ea213276be
SaerElMasri <saer1890@gmail.com>
2023-03-06 15:08:08 +0200

    Attached API of display products, filter products and add to cart to the product listing page

--- Js/product_browser.js
+++ Js/product_browser.js
@@ -1,13 +1,105 @@
 document.addEventListener('DOMContentLoaded', () => {
     const url = "http://localhost/backend-group-project2/products.php";
+    fetchProducts(url);
+});
+document.querySelectorAll(".add-to-cart-btn").onclick = () => {
+    console.log("Added");
+}
+let check_laptop = document.getElementById("laptops");
+let check_smartphone = document.getElementById("smarthphones");
+let check_acc = document.getElementById("accessories");
+document.getElementById("filter-action").onclick = () => {
+    document.querySelector(".products-div").innerHTML = "";
+    const filter_category = "http://localhost/backend-group-project2/category_product.php";
 
-    axios(url)
-    const fetchProducts = async(url) => {
-        await fetch(url).then((response)=>{
-            return response.json();
-        }).then(data => {
-            data.forEach(prod => {
-                const markup = `
+    if(check_laptop.checked){
+        axios.get(filter_category, {
+            params: {
+                "category": check_laptop.value
+            }
+        }).then((response) => {
+            let data = response.data;
+            products(data);
+        }).catch((err) => {
+            console.log(err);
+        });
+    }else if(check_smartphone.checked){
+        axios.get(filter_category, {
+            params: {
+                "category": check_smartphone.value
+            }
+        }).then((response) => {
+            let data = response.data;
+            products(data);
+        }).catch((err) => {
+            console.log(err);
+        });
+    }else if(check_acc.checked){
+        axios.get(filter_category, {
+            params: {
+                "category": check_acc.value
+            }
+        }).then((response) => {
+            let data = response.data;
+            products(data);
+        }).catch((err) => {
+            console.log(err);
+        });
+    }else{
+        alert("Nothing to filter");
+    }
+};
+document.getElementById("clear-action").onclick = () => {
+    const url = "http://localhost/backend-group-project2/products.php";
+    document.querySelector(".products-div").innerHTML = "";
+    fetchProducts(url);
+}
+
+
+const products = (data_response) => {
+    const addURL = "http://localhost/backend-group-project2/add_to_cart.php";
+    data_response.forEach((item) => {
+        const markup = `
+            <div class="product-component">
+            <div class="product-img">
+                <img src="../assets/dummy_product.png" alt="">
+                <button class="add-to-cart-btn">Add to Cart</button>
+                <div class="bg-effect"></div>
+            </div>
+            <div class="product-brief">
+                <h1>${item.name}</h1>
+                <h2>$${item.price}</h2>
+            </div>
+            </div>
+        `;
+        const element = document.createRange().createContextualFragment(markup);
+            const buttons = element.querySelectorAll('.add-to-cart-btn');
+            let prod_data = new FormData();
+                prod_data.append("id", item.id);
+                prod_data.append("prod_name", item.name);
+                prod_data.append("prod_description", item.description);
+                prod_data.append("price", item.price);
+                prod_data.append("image_url", item.img_url);
+            buttons.forEach((button) => {
+                button.addEventListener('click', () => {
+                    axios.post(addURL, prod_data).then((res) => {
+                        console.log(res['status']);    
+                    }).catch((err) => {
+                        console.log(err);
+                    });
+                });
+            });
+            document.querySelector(".products-div").appendChild(element);
+    });
+}
+const fetchProducts = async(url) => {
+    const addURL = "http://localhost/backend-group-project2/add_to_cart.php";
+    await fetch(url).then((response)=>{
+        return response.json();
+    }).then(data => {
+        data.forEach(prod => {
+            let id = prod.id;
+            const markup = `
                 <div class="product-component">
                 <div class="product-img">
                     <img src="../assets/dummy_product.png" alt="">
@@ -20,13 +112,27 @@ document.addEventListener('DOMContentLoaded', () => {
                 </div>
                 </div>
             `;
-            document.querySelector(".products-div").insertAdjacentHTML('beforeend', markup);
-                
+            const element = document.createRange().createContextualFragment(markup);
+            const buttons = element.querySelectorAll('.add-to-cart-btn');
+            let prod_data = new FormData();
+                prod_data.append("id", id);
+                prod_data.append("prod_name", prod.name);
+                prod_data.append("prod_description", prod.description);
+                prod_data.append("price", prod.price);
+                prod_data.append("image_url", prod.img_url);
+            buttons.forEach((button) => {
+                button.addEventListener('click', () => {
+                    axios.post(addURL, prod_data).then((res) => {
+                        console.log(res['status']);    
+                    }).catch((err) => {
+                        console.log(err);
+                    });
+                });
             });
-        }).catch((err) => {
-            console.log(err);
+            document.querySelector(".products-div").appendChild(element);
+            
         });
-
-    }
-    fetchProducts(url);
-});
+    }).catch((err) => {
+        console.log(err);
+    });
+}
