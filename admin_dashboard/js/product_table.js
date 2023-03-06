25755e90dd32709138606fd79bf73349a508bcd8
SaerElMasri <saer1890@gmail.com>
2023-03-07 01:44:41 +0200

    Added product add feature and edit product feature

--- admin_dashboard/js/product_table.js
+++ admin_dashboard/js/product_table.js
@@ -2,39 +2,32 @@ const logout_btn = document.querySelector("#logout_btn");
 logout_btn.addEventListener("click", () => {
     window.location.href="http://localhost/frontend-group-project2/pages/registration.html";
 });
-
 const product_api = "http://localhost/backend-group-project2/products.php";
 const fetchProducts = async(url) => {
+    let product_data = new FormData();
     await fetch(url).then((response)=>{
         return response.json();
     }).then(data => {
         data.forEach(prod => {
-            console.log(prod.id);
             const markup = `
                 <div class="row-product">
                     <h1>${prod.name}</h1>
                     <h1>${prod.description}</h1>
                     <h1>$${prod.price}</h1>
                     <h1>${prod.category}</h1>
-                    <button type="button" id="edit-action">Edit Product</button>
+                    <button type="button" id="edit-action" data-id="${prod.id}">Edit Product</button>
                 </div>
             `;
+            document.querySelector(".row").insertAdjacentHTML("beforeend",markup);
+        } );
             const edit_btn = document.querySelectorAll("#edit-action");
-            let product_data = new FormData();
-            product_data.append("id", prod.id);
-            product_data.append("name", prod.name);
-            product_data.append("description", prod.description);
-            product_data.append("price", prod.price);
-            product_data.append("category", prod.category);
-            product_data.append("image_url", prod.image_url);
             edit_btn.forEach((btn) => {
                 btn.addEventListener("click", () => {
-                    window.location.href = `http://localhost/frontend-group-project2/pages/edit_product.htmlid=${id}`;
+                    const productId = btn.getAttribute("data-id");
+                    window.location.href = `http://localhost/frontend-group-project2/admin_dashboard/pages/edit_product.html?id=${productId}`;
                 });
             })
-            document.querySelector(".row").insertAdjacentHTML("beforeend",markup);
-        });
-    }).catch((err) => {
+        }).catch((err) => {
         console.log(err);
     });
 }
