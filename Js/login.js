ffba193040aa2943db8d98b59d6007ffc6916aa3
rima <rima.abouchakra.gms@gmail.com>
2023-03-06 21:58:24 +0200

    redirected admin to product table

--- Js/login.js
+++ Js/login.js
@@ -79,7 +79,12 @@ function signin() {
         console.log(res.data)
     if (res.data.response== "logged in") {
       alert("Logged in successfully!");
-      window.location.href = "../index.html"; // redirect to product page
+      if(email==="admin@gmail.com"&&password==="admin"){
+         window.location.href = "../admin_dashboard/pages/product_table.html";
+      }else
+        window.location.href = "../index.html"; // redirect to product page
+      
+      
       
     }else {
       alert("Make sure login information is correct");
